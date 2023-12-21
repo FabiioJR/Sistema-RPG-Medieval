c7ab15e8ebbe18c199c6baab0de18bbe172e7e1c
Fábio Almeida <145670931+FabiioJR@users.noreply.github.com>
2023-12-21 13:53:59 -0300

    guerreiro.py

---
+++ guerreiro.py
@@ -0,0 +1,15 @@
+from criatura import Criatura
+
+class Guerreiro(Criatura):
+    def __init__(self, nome, pontos_vida, ataque, defesa, arma):
+        super().__init__(nome, pontos_vida, ataque, defesa)
+        self._arma = arma
+    
+    
+    def atacar(self):
+        return self._ataque + self._ataque * self._arma * 0.3
+    
+    def defender(self):
+        self._defesa += 25
+        if self._defesa > 300:
+            self._defesa = 300
